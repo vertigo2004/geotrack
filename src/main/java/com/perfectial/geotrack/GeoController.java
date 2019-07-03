30cd64237636fe9136cde6183c5efcedda681723
Oleh Pashkevych <vertigo.if@gmail.com>
2019-07-03 17:48:48 +0300

    Implemented as one-page.

--- src/main/java/com/perfectial/geotrack/GeoController.java
+++ src/main/java/com/perfectial/geotrack/GeoController.java
@@ -10,10 +10,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -23,13 +26,21 @@ public class GeoController {
     @Autowired
     GpxParser gpxParser;
 
-    @PostMapping("gpx-load")
-    public String processGPX(@RequestParam("file") MultipartFile file, Model model) {
+    @GetMapping("/")
+    public String showMap(Model model)  throws IOException {
+
+        return "map";
+    }
+
+    @PostMapping("/")
+    public String processGPX(@RequestParam("file") MultipartFile file, RedirectAttributes redirectAttributes) {
 
         log.info("ContentType: {}", file.getContentType());
 
         log.info("Name: {}", file.getName());
         log.info("OriginalFilename: {}", file.getOriginalFilename());
+        redirectAttributes.addFlashAttribute("message",
+                "You successfully uploaded " + file.getOriginalFilename() + "!");
 
         final GpxContentHandler gch = new GpxContentHandler();
 
@@ -43,13 +54,12 @@ public class GeoController {
 
             try {
                 String json = objectMapper.writeValueAsString(track);
-                log.info(json);
-                model.addAttribute("lonLat", json);
+                redirectAttributes.addFlashAttribute("lonLat", json);
             } catch (JsonProcessingException e) {
                 throw new GpxException("Error parsing GPX to JSON");
             }
         }
-        model.addAttribute("message", "File " + file.getOriginalFilename() + " is loaded!");
-        return "map";
+
+        return "redirect:/";
     }
 }
